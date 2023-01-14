524906fd12b57f68bca786fef0a372ff9b26604c
RuiOkazaki <okazaki200226@gmail.com>
2023-01-14 22:27:32 +0900

    chore: add google fonts

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,18 +1,22 @@
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import { ColorModeScript } from '@chakra-ui/react'
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import { ColorModeScript } from '@chakra-ui/react';
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html>
-        <Head />
+        <Head>
+          <link
+            href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&family=Noto+Sans+JP:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
-          {/* Make Color mode to persists when you refresh the page. */}
           <ColorModeScript />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,39 +1,18 @@
-import { extendTheme } from '@chakra-ui/react'
-
-const fonts = { mono: `'Menlo', monospace` }
-
-const breakpoints = {
-  sm: '40em',
-  md: '52em',
-  lg: '64em',
-  xl: '80em',
-}
+import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        'font-family': ['Montserrat', 'Noto Sans JP', 'sans-serif'].join(','),
+      },
+    },
+  },
   semanticTokens: {
     colors: {
-      text: {
-        default: '#16161D',
-        _dark: '#ade3b8',
-      },
-      heroGradientStart: {
-        default: '#7928CA',
-        _dark: '#e3a7f9',
-      },
-      heroGradientEnd: {
-        default: '#FF0080',
-        _dark: '#fbec8f',
-      },
-    },
-    radii: {
-      button: '12px',
+      text: '#16161D',
     },
   },
-  colors: {
-    black: '#16161D',
-  },
-  fonts,
-  breakpoints,
-})
+});
 
-export default theme
+export default theme;
