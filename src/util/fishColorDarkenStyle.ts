b1ef481679903a7f521bd0355312ae66cd9a54db
RuiOkazaki <okazaki200226@gmail.com>
2023-01-16 05:43:56 +0900

    style: webkitを足したけどあまり意味はなさそう

--- src/util/fishColorDarkenStyle.ts
+++ src/util/fishColorDarkenStyle.ts
@@ -1,5 +1,7 @@
-const darken = () => ({
-  filter: 'brightness(0.7)',
-});
+const darken = {
+  filter: 'brightness(70%)',
+  '-webkit-filter': 'brightness(70%)',
+  willChange: 'filter',
+};
 
 export default darken;
