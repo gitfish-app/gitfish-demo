96395adfb299ab0f1dcfff0298f85aecb4c07741
renmurakami <autoeyy@gmail.com>
2023-01-18 21:06:48 +0900

    style: 背景色を変更

--- src/pages/pc/index.tsx
+++ src/pages/pc/index.tsx
@@ -2,13 +2,14 @@ import { Box } from '@chakra-ui/react';
 import { NextPage } from 'next';
 import { useState } from 'react';
 import Fish from '../../components/pc/Character';
+import FishPopover from '../../components/pc/FishPopover/FishPopover';
 
 const Aquarium: NextPage = () => {
   const [aquariumWidth, setAquariumWidth] = useState(800);
   const [aquariumHeight, setAquariumHeight] = useState(600);
 
   return (
-    <div>
+    <Box bgColor={'#050732'}>
       <Box
         pos={'relative'}
         w={aquariumWidth}
@@ -20,6 +21,14 @@ const Aquarium: NextPage = () => {
           characterMoveDistance={{ x: 100, y: 100 }}
           aquariumSize={{ width: aquariumWidth, height: aquariumHeight }}
         />
+        <Box pos={'absolute'}>
+          <FishPopover
+            fishName="test"
+            commitCount={30}
+            mainLanguage={'JavaScript'}
+            languagePercentage={50}
+          />
+        </Box>
       </Box>
       <button onClick={() => setAquariumWidth((width) => width + 100)}>
         Change width
@@ -27,7 +36,7 @@ const Aquarium: NextPage = () => {
       <button onClick={() => setAquariumHeight((height) => height + 100)}>
         Change height
       </button>
-    </div>
+    </Box>
   );
 };
 
