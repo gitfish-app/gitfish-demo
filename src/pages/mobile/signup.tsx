942b0819bfbd328a0124a703cd243e4080ab58ce
renmurakami <autoeyy@gmail.com>
2023-01-27 09:48:57 +0900

    style: signupページのUI完成

--- src/pages/mobile/signup.tsx
+++ src/pages/mobile/signup.tsx
@@ -1,4 +1,4 @@
-import { Box, Image } from '@chakra-ui/react';
+import { Box, Image, Text } from '@chakra-ui/react';
 import { FC } from 'react';
 import FormButton from '../../components/mobile/Auth/FormButton';
 import MobileWrap from '../../components/mobile/MobileWrap';
@@ -38,11 +38,20 @@ const SignUp: FC = () => {
           />
         </AbsoluteButton>
       </Box>
+      <Box w={'100%'} h={'127px'} transform={'translateY(-28px)'}>
+        <Image src={'/assets/mobile-auth-bg-image.png'} w={'100%'} h={'100%'} />
+      </Box>
       <Box
-        mt={'160px'}
+        transform={'translateY(-46px)'}
         borderTopLeftRadius={'18px'}
         borderTopRightRadius={'18px'}
+        bgColor={'#0E2144'}
+        pt={'32px'}
+        px={'30px'}
       >
+        <Text fontSize={'30px'} fontWeight={'bold'} color={'#D3EDFB'}>
+          Sign up
+        </Text>
         <FormButton
           text={'Continue with GitHub'}
           fontSize={'16px'}
@@ -54,6 +63,7 @@ const SignUp: FC = () => {
           display={'flex'}
           justifyContent={'center'}
           alignItems={'center'}
+          mt={'32px'}
           LeftElement={
             <Box
               as={'i'}
@@ -64,15 +74,17 @@ const SignUp: FC = () => {
             />
           }
         />
-        <FormButton
-          text={'Sign Up'}
-          fontSize={'16px'}
-          color={'#D3EDFB'}
-          bgColor={'#006EFB'}
-          w={'100%'}
-          height={'55px'}
-          fontWeight={'bold'}
-        />
+        <Text
+          fontSize={'14px'}
+          color={'#707F95'}
+          textAlign={'center'}
+          mt={'30px'}
+        >
+          Already have an account?{' '}
+          <Box as={'span'} color={'#006EFB'}>
+            Sign In
+          </Box>
+        </Text>
       </Box>
     </MobileWrap>
   );
