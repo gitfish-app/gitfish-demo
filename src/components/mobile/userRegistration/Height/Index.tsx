598058f5083e847388aa4815818939014e32b3c0
renmurakami <autoeyy@gmail.com>
2023-01-28 12:38:21 +0900

    style: height page

--- src/components/mobile/userRegistration/Height/Index.tsx
+++ src/components/mobile/userRegistration/Height/Index.tsx
@@ -1,7 +1,32 @@
+import { useRouter } from 'next/router';
 import { FC } from 'react';
+import AbsoluteBox from '../../../reuse/AbsoluteBox';
+import DefaultButton from '../../../reuse/DefaultButton';
+import ProgressHeader from '../ProgressHeader';
+import Title from '../Title';
 
 const Index: FC = () => {
-  return <></>;
+  const router = useRouter();
+  return (
+    <>
+      <ProgressHeader progress={2} router={router} />
+      <Title title={'What’s Your Height?'} />
+      <AbsoluteBox
+        w={'100%'}
+        px={'30px'}
+        display={'flex'}
+        flexDirection={'column-reverse'}
+        pos={'absolute'}
+        bottom={'150px'}
+        zIndex={'20'}
+      >
+        <DefaultButton
+          text={'Next'}
+          onClick={() => router.push('/mobile/user-registration/height')}
+        />
+      </AbsoluteBox>
+    </>
+  );
 };
 
 export default Index;
